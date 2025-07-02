e25e52436113a171376e51ae649f71ac3cd5f73a
denisrudkov <rudkovdenis@outlook.com>
2025-07-02 10:20:00 +0000

    feat: LedgerView contract structure with roles

---
+++ src/LedgerView.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+
+import "./types/LedgerTypes.sol";
+import "./interfaces/ILedgerModule.sol";
+import "./interfaces/IRegistryModule.sol";
+import "./interfaces/IClassifierModule.sol";
+import "./interfaces/IIntegrationModule.sol";
+
+contract LedgerView is
+    Initializable,
+    AccessControlUpgradeable,
+    UUPSUpgradeable,
+    ReentrancyGuardUpgradeable,
+    ILedgerModule,
+    IRegistryModule,
+    IClassifierModule,
+    IIntegrationModule
+{
+    bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
+    bytes32 public constant OPERATOR_ROLE = keccak256("OPERATOR_ROLE");
+    bytes32 public constant CLASSIFIER_ROLE = keccak256("CLASSIFIER_ROLE");
+    bytes32 public constant INTEGRATOR_ROLE = keccak256("INTEGRATOR_ROLE");
+}
